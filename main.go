3b524e1ac3ecfe464b328fe6f033a734c1a21cff
lyaylyamiftahova <lyaylyamiftakhova@gmail.com>
2023-10-31 23:42:03 +0300

    add solution task

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	n := 56322547
+	max := 0
+
+	nStr := strconv.Itoa(n)
+
+	var digitStrings []string
+
+	for i := 0; i < len(nStr); i++ {
+		digitStrings = append(digitStrings, string(nStr[i]))
+	}
+
+	var digits []int
+
+	for _, digitStr := range digitStrings {
+		digit, _ := strconv.Atoi(digitStr)
+		digits = append(digits, digit)
+	}
+
+	for _, digit := range digits {
+		if digit > max {
+			max = digit
+		}
+	}
+	fmt.Println(max)
+}
