23b12535abb6941d6b89f8447b58a039b2224e0a
lyaylyamiftahova <lyaylyamiftakhova@gmail.com>
2023-12-14 22:43:06 +0300

    add task solution

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module example.com/goeducation
 
 go 1.21.3
 
-require github.com/go-chi/chi/v5 v5.0.10
+require github.com/go-chi/chi v1.5.5
--- myFirstServer/firstServer.go
+++ myFirstServer/firstServer.go
@@ -1,37 +1,89 @@
 package myFirstServer
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"strconv"
+	"sync"
 )
 
-var tempStorage = make(map[string]string)
+var mu sync.Mutex
+var helloMap = make(map[string]int)
 
 func MyFirstServer() {
-
+	// Создаем новый роутер с использованием go-chi
 	r := chi.NewRouter()
 
+	// Добавляем middleware для логирования
 	r.Use(middleware.Logger)
 
-	r.Get("/", sayHello)
+	// Регистрируем обработчики для каждой из трех ручек
+	r.Get("/hello/{name}", sayHello)
+	r.Get("/hello/count", getCount)
+	r.Put("/hello/{deleteCount}", updateCount)
 
-	r.Put("/greeting/{text}", updateGreeting)
-
-	tempStorage["greeting"] = "hello students!"
-
+	// Запускаем сервер на порту 3000
+	fmt.Println("Server is running on :3000")
 	http.ListenAndServe(":3000", r)
 }
 
+// Обработчик для ручки GET /hello/{name}
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(tempStorage["greeting"]))
+	// Получаем значение параметра {name} из URL
+	name := chi.URLParam(r, "name")
+
+	// Увеличиваем счетчик для данного имени
+	helloMap[name]++
+	// Получаем текущее количество запросов для данного имени
+	count := helloMap[name]
+
+	// Формируем JSON-ответ
+	response := map[string]interface{}{"Hello": name, "Count": count}
+	// Устанавливаем заголовок Content-Type
+	w.Header().Set("Content-Type", "application/json")
+	// Кодируем и отправляем JSON-ответ
+	json.NewEncoder(w).Encode(response)
 }
 
-func updateGreeting(w http.ResponseWriter, r *http.Request) {
-	greeting := chi.URLParam(r, "text")
+// Обработчик для ручки GET /hello/count
+func getCount(w http.ResponseWriter, r *http.Request) {
+	// Вычисляем общее количество запросов
+	var totalCount int
+	for _, count := range helloMap {
+		totalCount += count
+	}
 
-	tempStorage["greeting"] = greeting
+	// Формируем JSON-ответ с общим количеством запросов
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]int{"TotalCount": totalCount})
+}
 
-	w.Write([]byte("успех!"))
+// Обработчик для ручки PUT /hello/{deleteCount}
+func updateCount(w http.ResponseWriter, r *http.Request) {
+	// Получаем значение параметра {deleteCount} из URL
+	deleteCountParam := chi.URLParam(r, "deleteCount")
+	// Преобразуем его в число
+	deleteCount, err := strconv.Atoi(deleteCountParam)
+	if err != nil {
+		// В случае ошибки возвращаем ошибку BadRequest
+		http.Error(w, "Invalid deleteCount parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Вычисляем общее количество запросов
+	var totalCount int
+	for _, count := range helloMap {
+		totalCount += count
+	}
+
+	// Уменьшаем счетчики для всех имен пропорционально
+	for name := range helloMap {
+		helloMap[name] = helloMap[name] * (totalCount - deleteCount) / totalCount
+	}
+
+	// Отправляем успешный ответ
+	w.WriteHeader(http.StatusOK)
 }
