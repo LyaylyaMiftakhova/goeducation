ddea16e83ff3c6e023cb56d337e570079ac17a91
lyaylyamiftahova <lyaylyamiftakhova@gmail.com>
2024-02-13 13:54:15 +0300

    added comments

--- forPersonalTraining/ifElse.go
+++ forPersonalTraining/ifElse.go
@@ -2,6 +2,11 @@ package forPersonalTraining
 
 import "fmt"
 
+/*Реализуйте функцию DomainForLocale(domain, locale string) string,
+которая добавляет язык locale как поддомен к домену domain.
+Язык может прийти пустым, тогда нужно добавить поддомен en..
+*/
+
 func DomainForLocale(domain, locale string) string {
 	if locale == "" {
 		locale = "en"
